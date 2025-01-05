0d005966201ccf383f9070be4301228ecb6b2149
emailsarenice <emailsarenice@gmail.com>
2025-01-05 13:30:16 +1030

    initial setups

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# React + Vite
+# Tunes
+
+This is a demo project to help me learn React. It uses Spotify's API to search for songs some other functionalities.
+
+# Vite Notes
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/main.scss'
+import 'normalize.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
